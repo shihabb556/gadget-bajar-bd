de60bd1009a35d9a1f67ef746db2de4af593ebe1
shihabb556 <shihabaly556@gmail.com>
2026-07-03 18:22:42 +0600

    feat: add logging for Pixel ID and fbq in PixelPageTracker; update Content-Security-Policy for Facebook integration

--- next.config.ts
+++ next.config.ts
@@ -35,7 +35,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://connect.facebook.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https://res.cloudinary.com https://images.unsplash.com https://www.facebook.com; connect-src 'self' https://api.cloudinary.com data: blob: https://graph.facebook.com;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://connect.facebook.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https://connect.facebook.net https://res.cloudinary.com https://images.unsplash.com https://www.facebook.com; connect-src 'self' https://api.cloudinary.com data: blob: https://graph.facebook.com; frame-src 'self' https://www.facebook.com; object-src 'none';",
           },
         ],
       },
